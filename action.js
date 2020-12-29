5e28d26d0dcdae80d72293db573d152480ddc7fc
desmon91 <48699667+desmon91@users.noreply.github.com>
2020-12-29 11:07:06 +0800

    Add files via upload
    changing animation and skill color

--- action.js
+++ action.js
@@ -3,16 +3,25 @@ let text = "Hi, my name is Desmond.";
 let speed = 125;
 const skills = document.querySelectorAll(".skill")
 const contact = document.querySelector(".contact_content")
+let animate = true;
 
 window.addEventListener('scroll', () => {
     let scrollValue = window.pageYOffset / document.body.offsetHeight
-    if(scrollValue > 0.5){
-        for (skill of skills){
-            skill.style.animation = "flicker-in 4s linear both";
+    function getRandomInt(max) {
+        return Math.floor(Math.random() * Math.floor(max));
+      }
+    let animationArr = []
+    for(let i = 0; i < skills.length; i++ ){
+        animationArr.push(getRandomInt(3))
+    }
+    if(scrollValue > 0.5 && animate){
+        for(let i = 0; i < skills.length; i++ ){
+             skills[i].style.animation = `flicker-in${animationArr[i]} 3.5s linear both`;
         }
+        animate = false
     }
     if (scrollValue > 0.8){
-            contact.style.animation = "fade-in 4s cubic-bezier(0.250, 0.460, 0.450, 0.940) both";
+           return contact.style.animation = "fade-in 3s linear both";
     }
    
   });
